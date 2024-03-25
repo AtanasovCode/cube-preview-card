f211e671f6a853702f04775fbcb23b2499114418
AtanasovCode <a_ljupce@yahoo.com>
2024-03-25 13:47:43 +0100

    created full design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import image from "./assets/image-equilibrium.jpg";
+import eth from "./assets/icon-ethereum.svg";
+import clock from "./assets/icon-clock.svg";
+import avatar from "./assets/image-avatar.png";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-background min-h-screen flex items-center justify-center">
+      <div
+        className="
+        bg-card flex flex-col max-w-[90%] p-6 rounded-xl text-white
+        md:max-w-[50%] lg:max-w-[25%]
+      "
+      >
+        <div className="rounded-xl overflow-hidden">
+          <img src={image} alt="image preview of nft" />
+        </div>
 
-export default App
+        <div className="mt-5">
+          <div className="font-bold text-xl mb-4">Equilibrium #3429</div>
+          <div className="text-softBlue mb-4">
+            Our equilibrium collection promotes balance and calm.
+          </div>
+
+          <div className="flex items-center justify-between mb-8">
+            <div className="flex items-center justify-center text-cyan">
+              <img src={eth} alt="ethereum icon" className="mr-3" />
+              <div>0.041 ETH</div>
+            </div>
+
+            <div className="flex items-center justify-center text-softBlue">
+              <img src={clock} alt="icon of a clock" className="mr-3" />
+              <div>3 days left</div>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-start mb-5">
+            <img
+              src={avatar}
+              alt="avatar image"
+              className="mr-3 w-8 rounded-full border border-solid border-white"
+            />
+            <div className="text-white">
+              <span className="text-softBlue">Creation of</span> Jules Wyvern
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      softBlue: "#8bb7d7",
+      cyan: "#00ffff",
+      background: "#0d1b27",
+      card: "#162a38",
+      veryDarkBlueLine: "#3c4d5f",
+      white: "#ffffff",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
